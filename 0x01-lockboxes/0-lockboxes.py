62aca1c0fae9b8790926ee8439baa2e177c325d2
Kirito99x <msaifeldin46@gmail.com>
2024-07-04 14:22:43 +0200

    fixed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-""" unlock Lockboxes """
-def canUnlockAll(boxes):
-    """unlock Lockboxes"""
+""" Lockboxes module"""
+def can_unlock_all(boxes):
+    """Function that determines if all the boxes can be opened"""
     if not boxes:
-        """if boxes is empty return false"""
         return False
     
-    n = len(boxes) # number of boxes
-    opended = [False] * n # list of boxes that are opend 
-    opended[0] = True # first box is always open
-    queue = [0] # list of boxes to open
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True
+    queue = [0]
 
     while queue:
-        """queue of boxes to open"""
-        current_box = queue.pop(0) # current box to open 
-        for key in boxes[current_box]: # loop through keys in the current box
-            if key < n and not opended[key]: # check if key is valid and not opened
-                opended[key] = True # open the box
-                queue.append(key) # add the box to the queue
-    
-    return all(opended)
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if key < n and not opened[key]:
+                opened[key] = True
+                queue.append(key)
+
+    return all(opened)
