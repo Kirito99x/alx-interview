40ade92886a0c612359c3403d67a3b12f32ea39c
Kirito99x <msaifeldin46@gmail.com>
2024-07-04 14:27:11 +0200

    done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""lockboxes module"""
-def can_unlock_all(boxes):
-    """Unlock Lockboxes"""
+""" unlock Lockboxes """
+def canUnlockAll(boxes):
+    """unlock Lockboxes"""
     if not boxes:
-        """If boxes is empty, return False"""
+        """if boxes is empty return false"""
         return False
     
-    n = len(boxes)  # Number of boxes
-    opened = [False] * n  # List of boxes that are opened 
-    opened[0] = True  # First box is always open
-    queue = [0]  # List of boxes to open
+    n = len(boxes) # number of boxes
+    opended = [False] * n # list of boxes that are opend 
+    opended[0] = True # first box is always open
+    queue = [0] # list of boxes to open
 
     while queue:
-        """Queue of boxes to open"""
-        current_box = queue.pop(0)  # Current box to open 
-        for key in boxes[current_box]:  # Loop through keys in the current box
-            if key < n and not opened[key]:  # Check if key is valid and not opened
-                opened[key] = True  # Open the box
-                queue.append(key)  # Add the box to the queue
-
-    return all(opened)
+        """queue of boxes to open"""
+        current_box = queue.pop(0) # current box to open 
+        for key in boxes[current_box]: # loop through keys in the current box
+            if key < n and not opended[key]: # check if key is valid and not opened
+                opended[key] = True # open the box
+                queue.append(key) # add the box to the queue
+    
+    return all(opended)
