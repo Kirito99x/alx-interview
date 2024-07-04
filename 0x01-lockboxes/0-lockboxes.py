b9ab9ec7048b57f768fa517eca88d9e6bee00644
Kirito99x <msaifeldin46@gmail.com>
2024-07-04 14:29:19 +0200

    yes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,18 +5,17 @@ def canUnlockAll(boxes):
     if not boxes:
         """if boxes is empty return false"""
         return False
-    
     n = len(boxes) # number of boxes
-    opended = [False] * n # list of boxes that are opend 
+    opended = [False] * n # list of boxes that are opend
     opended[0] = True # first box is always open
     queue = [0] # list of boxes to open
 
     while queue:
         """queue of boxes to open"""
-        current_box = queue.pop(0) # current box to open 
+        current_box = queue.pop(0) # current box to open
         for key in boxes[current_box]: # loop through keys in the current box
             if key < n and not opended[key]: # check if key is valid and not opened
                 opended[key] = True # open the box
-                queue.append(key) # add the box to the queue
-    
+                queue.append(key) #add the box to the queue
+
     return all(opended)
