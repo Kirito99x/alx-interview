fc05da4163e1ff2942b9bb5b89c029d23a11a41d
Kirito99x <msaifeldin46@gmail.com>
2024-07-12 03:49:31 +0200

    fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,16 +2,19 @@ def minOperations(n):
     '''find the minimum numbers of operations needed to result in n H characters.'''
     if not isinstance(n, int):
         return 0
-    opertions_count = 0
-    clipboard = 0
-    done = 1
-
-    while done < n:
-        if n % done == 0:
-            cp_pa = done
-            done += cp_pa
-        else:
-            done += 1
-        opertions_count += 1
-
-    return opertions_count
+    num_ops = 0
+    cp_pa = 0
+    Done = 1
+    while Done < n:
+        if cp_pa == 0:
+            cp_pa = Done
+            Done += cp_pa
+            num_ops += 2
+        elif n - Done > 0 and (n - Done) % Done == 0:
+            cp_pa = Done
+            Done += cp_pa
+            num_ops += 2
+        elif cp_pa > 0:
+            Done += cp_pa
+            num_ops += 1
+    return num_ops
